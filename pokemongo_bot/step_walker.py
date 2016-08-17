fdeac4395a5482e3a3282175f8d60770ae63fbc1
Alex Yao <alexyaoyang@hotmail.com>
2016-08-17 16:06:25 -0700

    Remove commit from another PR

--- pokemongo_bot/step_walker.py
+++ pokemongo_bot/step_walker.py
@@ -19,7 +19,6 @@ class StepWalker(object):
             dest_lat,
             dest_lng
         )
-            
         self.speed = self.bot.config.walk_max - random() * (self.bot.config.walk_max - self.bot.config.walk_min)
 
         self.destLat = dest_lat
